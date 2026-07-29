90c79562376662138a653a5575bf8e7e8d634e87
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-28 21:43:09 -0300

    Atualizações de index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema MVC 1</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <!-- Cabeçalho -->
+    <header class="bg-dark text-white py-3">
+        <div class="container">
+            <div class="d-flex flex-column flex-md-row justify-content-between align-items-center">
+                <h1 class="h3 mb-3 mb-md-0"> Sistema de Cadastro </h1>
+
+                <!-- Menu principal -->
+                <nav class="nav">
+                    <a href="index.php?page=produtos" class="nav-link text-white"> Produtos </a>
+
+                    <!-- TODO: Criar para clientes e funcionarios -->
+
+                </nav>
+            </div>
+        </div>
+
+    </header>
+
+
+
+
+
+
+
+
+
+
+    <!-- JavaScript do Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
