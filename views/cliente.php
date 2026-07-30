b43deebda7e2447783fae95aa45a42cc11bdf959
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-30 20:57:02 -0300

    Atualizações gerais

---
+++ views/cliente.php
@@ -0,0 +1,52 @@
+<section>
+
+    <div class="col-md-6 mx-auto">
+
+        <h2> Cadastro de clientes </h2>
+
+        <!-- Formulário -->
+        <form id="formCliente">
+
+            <!-- Nome -->
+            <div class="mb-3">
+                <label for="nome"> Nome </label>
+                <input type="text" id="nome" name="nome" class="form-control">
+            </div>
+
+            <!-- CPF -->
+            <div class="mb-3">
+                <label for="cpf"> CPF </label>
+                <input type="text" id="cpf" name="cpf" class="form-control">
+            </div>
+
+            <!-- E-mail -->
+            <div class="mb-3">
+                <label for="email"> E-mail </label>
+                <input type="text" id="email" name="email" class="form-control">
+            </div>
+
+            <!-- Telefone -->
+            <div class="mb-3">
+                <label for="telefone"> Telefone </label>
+                <input type="text" id="telefone" name="telefone" class="form-control">
+            </div>
+
+
+            <!-- Botão cadastrar -->
+            <button type="submit" class="btn btn-primary w-100">
+                Cadastrar
+            </button>
+
+
+            <!-- Mensagem de retorno -->
+            <div id="mensagem" class="alert d-none mt-3"></div>
+
+
+        </form>
+
+
+    </div>
+
+    <script src="assets/js/cliente.js"></script>
+
+</section>
